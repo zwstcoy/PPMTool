0849d9de70c07ae919691c1ef78bfaaad013c866
Tong Zheng <tzheng@squarespace.com>
2020-05-20 22:22:27 -0400

    branch5-return error message

--- PPMTool/src/main/java/com/example/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -1,12 +1,17 @@
 package com.example.ppmtool.web;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +31,12 @@ public class ProjectController {
   @PostMapping("")
   public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
     if (result.hasErrors()){
-      return new ResponseEntity<String>("Invalid Project", HttpStatus.BAD_REQUEST);
+
+      Map<String, String> errorRespond = new HashMap<>();
+      for (FieldError error: result.getFieldErrors()){
+        errorRespond.put(error.getField(), error.getDefaultMessage());
+      }
+      return new ResponseEntity<Map<String, String>>(errorRespond, HttpStatus.BAD_REQUEST);
     }
     Project project1 = projectService.saveOrUpdateProject(project);
     return new ResponseEntity<Project>(project, HttpStatus.CREATED);
