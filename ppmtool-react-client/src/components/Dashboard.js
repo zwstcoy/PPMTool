c56e0f1a2eb750944d39dc95f1738fe630f97815
Tong Zheng <tzheng@squarespace.com>
2020-05-21 16:30:42 -0400

    branch15

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -4,10 +4,23 @@ import ProjectItem from "./Project/ProjectItem";
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1 className="alert alert-warning">Welcome to Dashboard</h1>
-        <ProjectItem />
-      </div>
+        <div className="projects">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Projects</h1>
+                    <br />
+                    <a href="ProjectForm.html" className="btn btn-lg btn-info">Create a Project</a>
+                    <br />
+                    <hr />
+
+                    <ProjectItem />
+
+                </div>
+            </div>
+        </div>
+    </div>
+
     );
   }
 }
