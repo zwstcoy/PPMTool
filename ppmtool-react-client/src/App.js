374e7cdb123f3a3be7f1722caec70cb9715d1c9d
Tong Zheng <tzheng@squarespace.com>
2020-05-21 16:57:26 -0400

    branch16

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/bashborad" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class AddProject extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add project from</h1>
+            </div>
+        )
+    }
+}
+
+
+export default AddProject;
