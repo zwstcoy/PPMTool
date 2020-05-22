a15c8a4a7b79e36b6f102e3b22c6bde676959527
Tong Zheng <tzheng@squarespace.com>
2020-05-22 15:50:45 -0400

    branch47

--- ppmtool-react-client/src/actions/BacklogAction.js
+++ ppmtool-react-client/src/actions/BacklogAction.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK} from "./Types";
+import { GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from "./Types";
 
-export const addProjectTask = (backlog_id, project_task, history)=> async dispatch =>{
-    await axios.post(`/api/backlog/${backlog_id}`);
-    history.push(`/projectBorad/$(backlog_id)`)
-
-}
+export const addProjectTask = (backlog_id, project_task, history) => async (
+  dispatch
+) => {
+  await axios.post(`/api/backlog/${backlog_id}`, project_task);
+  history.push(`/projectBoard/${backlog_id}`);
+};
--- ppmtool-react-client/src/components/ProjectBorad/ProjectTask/AddProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBorad/ProjectTask/AddProjectTask.js
@@ -18,6 +18,28 @@ class AddProjectTask extends Component {
       dueDate: null,
     };
 
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newProjectTask = {
+      summary: this.state.summary,
+      acceptCriteria: this.state.acceptCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate,
+    };
+    this.props.addProjectTask(
+      this.state.projectIdentifier,
+      newProjectTask,
+      this.props.history
+    );
   }
 
   render() {
@@ -33,7 +55,7 @@ class AddProjectTask extends Component {
               </Link>
               <h4 className="display-4 text-center">Add Project Task</h4>
               <p className="lead text-center">Project Name + Project Code</p>
-              <form>
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
@@ -41,14 +63,16 @@ class AddProjectTask extends Component {
                     name="summary"
                     placeholder="Project Task summary"
                     value={this.state.summary}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
                   <textarea
                     className="form-control form-control-lg"
-                    placeholder="Acceptance Criteria"
-                    name="acceptanceCriteria"
+                    placeholder="accept Criteria"
+                    name="acceptCriteria"
                     value={this.state.acceptCriteria}
+                    onChange={this.onChange}
                   ></textarea>
                 </div>
                 <h6>Due Date</h6>
@@ -58,6 +82,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="dueDate"
                     value={this.state.dueDate}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -65,6 +90,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="priority"
                     value={this.state.priority}
+                    onChange={this.onChange}
                   >
                     <option value={0}>Select Priority</option>
                     <option value={1}>High</option>
@@ -78,6 +104,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="status"
                     value={this.state.status}
+                    onChange={this.onChange}
                   >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
@@ -103,7 +130,4 @@ AddProjectTask.propTypes = {
   addProjectTask: PropTypes.func.isRequired,
 };
 
-// const mapStateToProps = (state) => ({
-//   project_task: state.errors,
-// });
-export default connect(null, addProjectTask)(AddProjectTask);
+export default connect(null, { addProjectTask })(AddProjectTask);
