07483c8e211eceb77fef6b91cd41f4111a2a3ca8
Tong Zheng <tzheng@squarespace.com>
2020-05-22 16:22:11 -0400

    branch49 refacter pt b

--- ppmtool-react-client/src/components/ProjectBorad/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBorad/Backlog.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ProjectBorad from "./ProjectBorad";
+import ProjectTask from "./ProjectTask/ProjectTask";
 
 class Backlog extends Component {
-    render() {
-        return (
-            <div>
-                
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card text-center mb-2">
+                <div className="card-header bg-secondary text-white">
+                  <h3>TO DO</h3>
+                </div>
+              </div>
+              <ProjectTask />
             </div>
-        )
-    }
+            <div className="col-md-4">
+              <div className="card text-center mb-2">
+                <div className="card-header bg-primary text-white">
+                  <h3>In Progress</h3>
+                </div>
+              </div>
+              <ProjectTask />
+            </div>
+            <div className="col-md-4">
+              <div className="card text-center mb-2">
+                <div className="card-header bg-success text-white">
+                  <h3>Done</h3>
+                </div>
+              </div>
+              <ProjectTask />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default  Backlog
+export default Backlog;
