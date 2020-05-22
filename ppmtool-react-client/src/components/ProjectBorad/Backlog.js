66eb810c5936c505fea914fc7ec7daf3be64e945
Tong Zheng <tzheng@squarespace.com>
2020-05-22 17:13:23 -0400

    branch52 add color

--- ppmtool-react-client/src/components/ProjectBorad/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBorad/Backlog.js
@@ -9,6 +9,25 @@ class Backlog extends Component {
     const tasks = project_tasks.map((project_task) => (
       <ProjectTask key={project_task.id} project_task={project_task} />
     ));
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(tasks[i]);
+
+      if (tasks[i].props.project_task.status === "TO_DO") {
+        todoItems.push(tasks[i]);
+      }
+
+      if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+        inProgressItems.push(tasks[i]);
+      }
+
+      if (tasks[i].props.project_task.status === "DONE") {
+        doneItems.push(tasks[i]);
+      }
+    }
     return (
       <div>
         <div className="container">
@@ -19,7 +38,7 @@ class Backlog extends Component {
                   <h3>TO DO</h3>
                 </div>
               </div>
-              {tasks}
+              {todoItems}
             </div>
             <div className="col-md-4">
               <div className="card text-center mb-2">
@@ -27,7 +46,7 @@ class Backlog extends Component {
                   <h3>In Progress</h3>
                 </div>
               </div>
-              {tasks}
+              {inProgressItems}
             </div>
             <div className="col-md-4">
               <div className="card text-center mb-2">
@@ -35,7 +54,7 @@ class Backlog extends Component {
                   <h3>Done</h3>
                 </div>
               </div>
-              {tasks}
+              {doneItems}
             </div>
           </div>
         </div>
--- ppmtool-react-client/src/components/ProjectBorad/ProjectTask/ProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBorad/ProjectTask/ProjectTask.js
@@ -1,30 +1,44 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 class ProjectTask extends Component {
   render() {
     const { project_task } = this.props;
-    return (
-      <div>
-        <div className="card mb-1 bg-light">
-          <div className="card-header text-primary">
-            ID: {project_task.id} -- Priority: {project_task.priority}
-          </div>
-          <div className="card-body bg-light">
-            <h5 className="card-title">{project_task.summary}</h5>
-            <p className="card-text text-truncate ">
-              {project_task.acceptCriteria}
-            </p>
-            <Link to="#" className="btn btn-primary">
-              View / Update
-            </Link>
+    let priorityString;
+    let priorityClass;
 
-            <button className="btn btn-danger ml-4">Delete</button>
-          </div>
+    if (project_task.priority === 1) {
+      priorityClass = "bg-danger text-light";
+      priorityString = "HIGH";
+    }
+
+    if (project_task.priority === 2) {
+      priorityClass = "bg-warning text-light";
+      priorityString = "MEDIUM";
+    }
+
+    if (project_task.priority === 3) {
+      priorityClass = "bg-info text-light";
+      priorityString = "LOW";
+    }
+
+    return (
+      <div className="card mb-1 bg-light">
+        <div className={`card-header text-primary ${priorityClass}`}>
+          ID: {project_task.projectSequence} -- Priority: {priorityString}
+        </div>
+        <div className="card-body bg-light">
+          <h5 className="card-title">{project_task.summary}</h5>
+          <p className="card-text text-truncate ">
+            {project_task.acceptanceCriteria}
+          </p>
+          <a href="" className="btn btn-primary">
+            View / Update
+          </a>
+
+          <button className="btn btn-danger ml-4">Delete</button>
         </div>
       </div>
     );
   }
 }
-
 export default ProjectTask;
