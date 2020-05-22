3332926325cb0b8d6264029426222e154e41672f
Tong Zheng <tzheng@squarespace.com>
2020-05-21 20:42:25 -0400

    branch23 render ui

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -6,11 +6,12 @@ import { connect } from "react-redux";
 import { getProjects } from "../actions/ProjectAction";
 
 class Dashboard extends Component {
-    componentDidMount(){
-        this.props.getProjects();
-    }
+  componentDidMount() {
+    this.props.getProjects();
+  }
 
-    render() {
+  render() {
+    const { projects } = this.props.project;
     return (
       <div className="projects">
         <div className="container">
@@ -21,7 +22,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map((project) => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
