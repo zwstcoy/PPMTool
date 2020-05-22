beb6ffc47183cc6ec449e9450e364a49675c158e
Tong Zheng <tzheng@squarespace.com>
2020-05-21 23:47:27 -0400

    fixed bug

--- ppmtool-react-client/src/actions/ProjectAction.js
+++ ppmtool-react-client/src/actions/ProjectAction.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./Types";
 
-export const createProject = (project, history) => async (dispatch) => {
+export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
-      payload: {},
+      payload: err.response.data
     });
   }
 };
+
 
 export const getProjects = () => async (dispatch) => {
   const res = await axios.get("http://localhost:8080/api/project/all");
@@ -28,7 +33,7 @@ export const getProject = (id, history) => async (dispatch) => {
       type: GET_PROJECT,
       payload: res.data,
     });
-  } catch (error) {
+  } catch (err) {
     history.push("/dashboard");
   }
 };
