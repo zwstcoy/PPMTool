3bac01489da975070bec641eb4bb744c5af15294
Tong Zheng <tzheng@squarespace.com>
2020-05-22 14:27:16 -0400

    branch43 add backlog type nad reducer

--- ppmtool-react-client/src/actions/Types.js
+++ ppmtool-react-client/src/actions/Types.js
@@ -4,3 +4,7 @@ export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
 
 
+// BACKLOG type
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
