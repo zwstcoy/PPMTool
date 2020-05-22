b653e561916755a5478170dad104e7e02ddae583
Tong Zheng <tzheng@squarespace.com>
2020-05-22 15:00:49 -0400

    branch45 add pt form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,6 +8,8 @@ import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBorad from "./components/ProjectBorad/ProjectBorad";
+import AddProjectTask from "./components/ProjectBorad/ProjectTask/AddProjectTask";
 
 class App extends Component {
   render() {
@@ -19,7 +21,8 @@ class App extends Component {
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
-
+            <Route exact path="/projectBoard/:id" component={ProjectBorad} />
+            <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
           </div>
         </Router>
       </Provider>
--- ppmtool-react-client/src/components/Project/ProjectItem.js
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -25,11 +25,11 @@ class ProjectItem extends Component {
             </div>
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
-                <a href="#">
+                <Link to={`/projectBoard/${project.projectIdentifier}`}>
                   <li className="list-group-item board">
                     <i className="fa fa-flag-checkered pr-1">Project Board </i>
                   </li>
-                </a>
+                </Link>
 
                 <React.Fragment>
                   <Link to={`/updateProject/${project.projectIdentifier}`}>
