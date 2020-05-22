af7f9dbdb7aafa3ee7abf2ee1fbdfe8e7c2c97b6
Tong Zheng <tzheng@squarespace.com>
2020-05-22 00:33:36 -0400

    refactor

--- ppmtool-react-client/src/actions/ProjectAction.js
+++ ppmtool-react-client/src/actions/ProjectAction.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./Types";
 
-export const createProject = (project, history) => async dispatch => {
+export const createProject = (project, history) => async (dispatch) => {
   try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    const res = await axios.post("/api/project", project);
     history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
-      payload: {}
+      payload: {},
     });
   } catch (err) {
     dispatch({
@@ -17,9 +17,8 @@ export const createProject = (project, history) => async dispatch => {
   }
 };
 
-
 export const getProjects = () => async (dispatch) => {
-  const res = await axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("/api/project/all");
   dispatch({
     type: GET_PROJECTS,
     payload: res.data,
@@ -28,7 +27,7 @@ export const getProjects = () => async (dispatch) => {
 
 export const getProject = (id, history) => async (dispatch) => {
   try {
-    const res = await axios.delete(`http://localhost:8080/api/project/${id}`);
+    const res = await axios.delete(`/api/project/${id}`);
     dispatch({
       type: GET_PROJECT,
       payload: res.data,
@@ -39,9 +38,11 @@ export const getProject = (id, history) => async (dispatch) => {
 };
 
 export const deleteProject = (id) => async (dispatch) => {
-  await axios.delete(`http://localhost:8080/api/project/${id}`);
-  dispatch({
-    type: DELETE_PROJECT,
-    payload: id,
-  });
+  if (window.confirm("Are you sure to delete Project?")) {
+    await axios.delete(`/api/project/${id}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id,
+    });
+  }
 };
